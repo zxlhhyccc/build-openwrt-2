094339f2f388ef7b71d58daf3f7723116e5c155b
CN_SZTL <cnsztl@project-openwrt.eu.org>
2020-05-01 10:47:10 +0800

    fix typo

--- clone_source.sh
+++ clone_source.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
-cd "/home/runner"
-
 git clone "https://github.com/project-openwrt/openwrt.git" --branch "openwrt-18.06" --single-branch "openwrt"
-pushd "/home/runner/openwrt"; ./scripts/feeds update -a; ./scripts/feeds update -a; popd
+pushd "openwrt"; ./scripts/feeds update -a; ./scripts/feeds update -a; popd
